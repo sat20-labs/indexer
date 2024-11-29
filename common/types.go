2eb1b79952ef770eb81876bc3e7e40387a038796
sharphill <cszyf@163.com>
2024-11-29 09:05:10 +0800

    modified

--- common/types.go
+++ common/types.go
@@ -64,7 +64,7 @@ const INVALID_ID = math.MaxUint64
 
 const ALL_TICKERS = "*"
 type TickerName struct {
-	Protocol string `json:"protocol"` // 默认是ordx, 可以是任意协议，brc-20, runes, 甚至是eth
+	Protocol string `json:"protocol,omitempty"` // 默认是ordx, 可以是任意协议，brc-20, runes, 甚至是eth
 	TypeName string `json:"type"`     // 默认是FT， ASSET_TYPE_FT
 	Name     string `json:"ticker"`   // * 所有ticker
 }
