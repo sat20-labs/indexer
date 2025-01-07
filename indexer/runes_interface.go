49ccea79824cba131735dc3140d0b996c4404e3b
softwarecheng <softwarecheng@126.com>
2025-01-07 12:17:51 +0800

    coding

--- indexer/runes_interface.go
+++ indexer/runes_interface.go
@@ -4,10 +4,7 @@ import (
 	"github.com/sat20-labs/indexer/common"
 )
 
-
-
-
-func (b *IndexerMgr) GetRunesTickerMapV2() (map[string]*common.TickerInfo) {
+func (b *IndexerMgr) GetRunesTickerMapV2() map[string]*common.TickerInfo {
 	result := make(map[string]*common.TickerInfo)
 	tickers := b.RunesIndexer.GetAllTickers()
 	for _, tickerName := range tickers {
@@ -19,9 +16,8 @@ func (b *IndexerMgr) GetRunesTickerMapV2() (map[string]*common.TickerInfo) {
 	return result
 }
 
-
 func (p *IndexerMgr) GetRunesTickerV2(tickerName string) *common.TickerInfo {
-	ticker := p.RunesIndexer.GetRuneInfoWithName(tickerName)
+	ticker := p.RunesIndexer.GetRuneInfoWithId(tickerName)
 	if ticker == nil {
 		return nil
 	}
@@ -32,19 +28,18 @@ func (p *IndexerMgr) GetRunesTickerV2(tickerName string) *common.TickerInfo {
 	result.DisplayName = ticker.Name
 	result.Id = int64(ticker.Number)
 	result.Divisibility = int(ticker.Divisibility)
-	
+
 	result.TotalMinted = ticker.Supply.String()
 	result.MaxSupply = ticker.MaxSupply.String()
 	if ticker.MintInfo != nil {
 		result.Limit = ticker.MintInfo.Amount.String()
 	}
 	result.SelfMint = int(ticker.PreminePercentage)
-	
-	
+
 	result.DeployHeight = ticker.BlockHeight()
 	result.DeployBlocktime = int64(ticker.Timestamp)
 	result.DeployTx = ticker.Etching
-	
+
 	_, holders := p.RunesIndexer.GetAllAddressBalances(ticker.Id, 0, 1)
 	result.HoldersCount = int(holders)
 	result.InscriptionId = ""
@@ -73,36 +68,34 @@ func (b *IndexerMgr) GetRunesDBVer() string {
 	return "1.0.0"
 }
 
-func (p *IndexerMgr) GetRunesMintHistoryWithAddress(addressId uint64, 
+func (p *IndexerMgr) GetRunesMintHistoryWithAddress(addressId uint64,
 	ticker string, start int, limit int) ([]*common.MintInfo, int) {
 	result := make([]*common.MintInfo, 0)
 	infos, total := p.RunesIndexer.GetAddressMintHistory(ticker, addressId, uint64(start), uint64(limit))
 	for _, info := range infos {
 		result = append(result, &common.MintInfo{
-			Id: int64(info.Number),
-			Address: p.GetAddressById(info.AddressId),
-			Amount: info.Amount.String(),
-			Height: int(info.Height),
+			Id:            int64(info.Number),
+			Address:       p.GetAddressById(info.AddressId),
+			Amount:        info.Amount.String(),
+			Height:        int(info.Height),
 			InscriptionId: info.Utxo,
 		})
 	}
 	return result, int(total)
 }
 
-
-func (p *IndexerMgr) GetRunesMintHistory( 
+func (p *IndexerMgr) GetRunesMintHistory(
 	ticker string, start int, limit int) ([]*common.MintInfo, int) {
 	result := make([]*common.MintInfo, 0)
 	infos, total := p.RunesIndexer.GetMintHistory(ticker, uint64(start), uint64(limit))
 	for _, info := range infos {
 		result = append(result, &common.MintInfo{
-			Id: int64(info.Number),
-			Address: p.GetAddressById(info.AddressId),
-			Amount: info.Amount.String(),
-			Height: int(info.Height),
+			Id:            int64(info.Number),
+			Address:       p.GetAddressById(info.AddressId),
+			Amount:        info.Amount.String(),
+			Height:        int(info.Height),
 			InscriptionId: info.Utxo,
 		})
 	}
 	return result, int(total)
 }
-
