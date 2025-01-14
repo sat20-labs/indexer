1aa8e21a1c7525f921a49f7343dd2043d0e3c624
sharphill <cszyf@163.com>
2025-01-14 11:03:18 +0800

    modified

--- indexer/interface_v2.go
+++ indexer/interface_v2.go
@@ -318,7 +318,7 @@ func (b *IndexerMgr) GetAssetSummaryInAddressV3(address string) map[common.Ticke
 	runesAsset := b.RunesIndexer.GetAddressAssets(b.rpcService.GetAddressId(address))
 	for _, v := range runesAsset {
 		tickName := common.TickerName{Protocol: common.PROTOCOL_NAME_RUNES, Type: common.ASSET_TYPE_FT, Ticker: v.Rune}
-		result[tickName] = common.NewDecimalFromUint128(v.Balance, 0)
+		result[tickName] = common.NewDecimalFromUint128(v.Balance, int(v.Divisibility))
 	}
 
 	plainUtxoMap := make(map[uint64]int64)
