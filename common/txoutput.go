8b786fb240c3e9886411b49fe7f0b92ba4157fa9
sharphill <cszyf@163.com>
2025-02-06 14:40:27 +0800

    modified

--- common/txoutput.go
+++ common/txoutput.go
@@ -265,12 +265,14 @@ func (p *TxOutput) Split(name *swire.AssetName, value, amt int64) (*TxOutput, *T
 		return nil, nil, err
 	}
 	n := asset.BindingSat
-	if n != 0 && amt%int64(n) != 0 {
-		return nil, nil, fmt.Errorf("amt must be times of %d", n)
-	}
-	requiredValue := GetBindingSatNum(amt, asset.BindingSat)
-	if requiredValue > value {
-		return nil, nil, fmt.Errorf("value too small")
+	if n != 0 {
+		if amt%int64(n) != 0 {
+			return nil, nil, fmt.Errorf("amt must be times of %d", n)
+		}
+		requiredValue := GetBindingSatNum(amt, asset.BindingSat)
+		if requiredValue > value {
+			return nil, nil, fmt.Errorf("value too small")
+		}
 	}
 
 	if asset.Amount < amt {
