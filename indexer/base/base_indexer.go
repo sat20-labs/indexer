1ce4bad2205180a4eb93f91628f9d40bd22399a7
sharphill <cszyf@163.com>
2025-03-10 14:13:22 +0800

    modified

--- indexer/base/base_indexer.go
+++ indexer/base/base_indexer.go
@@ -75,7 +75,7 @@ func NewBaseIndexer(
 	}
 
 	if chaincfgParam.Name != "mainnet" {
-		indexer.keepBlockHistory = 144 // testnet4的分岔很多也很长
+		indexer.keepBlockHistory = 72 // testnet4的分岔很多也很长
 	}
 
 	indexer.addressIdMap = make(map[string]*AddressStatus, 0)
