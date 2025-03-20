4fb907c4535215047c75df5fb8ec65dd04a512ea
sharphill <cszyf@163.com>
2025-03-20 10:09:02 +0800

    modified

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 rm indexer-mainnet
-go build -o indexer-mainnet
+go build -o indexer-mainnet  -ldflags="-s -w"
 
 if [ $# -eq 0 ]; then
   nohup ./indexer-mainnet -env ./mainnet.env > ./nohup_mainnet.log 2>&1 &
