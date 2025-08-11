5ab6737da81e44fe324c732e3d644be13986e96b
sharphill <cszyf@163.com>
2025-08-11 21:39:21 +0800

    modified

--- common/version.go
+++ common/version.go
@@ -10,4 +10,5 @@ const ORDX_INDEXER_VERSION = "1.0.2"
 // 1.3.0  2024.10.21    utxoId = channel short id
 // 1.4.0  2024.12.24    support STP
 // 1.5.0  2025.02.03    support n parameter
-const BASE_DB_VERSION = "1.5.0"
+// 1.6.0  2025.08.10    change db from badger to levelDB
+const BASE_DB_VERSION = "1.6.0"
