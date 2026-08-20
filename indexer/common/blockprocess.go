1298c018a14fe2ae283f1f4ff92495e3c1ab219e
yingfeng <zhengyingfeng@gmail.com>
2026-08-20 09:48:06 +0800

    modified

--- indexer/common/blockprocess.go
+++ indexer/common/blockprocess.go
@@ -2,8 +2,8 @@ package common
 
 import "github.com/sat20-labs/indexer/common"
 
-var STEP_RUN_MODE = false
-var CHECK_SELF = true
+var STEP_RUN_MODE = false // true: 模拟正常的服务状态更新数据。仅用于测试。生产环境设置为false。
+var CHECK_SELF = true // 当STEP_RUN_MODE为true时，最好设置为false。生产环境设置为true
 
 type BlockProcCallback interface {
 	PrepareUpdateTransfer(block *common.Block, coinbase []*common.Range)
